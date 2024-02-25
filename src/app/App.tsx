744c222043b25fa270b4636f14a8e278636ce9c4
andre <cherepanovandrey07@gmail.com>
2024-02-25 14:18:19 +0300

    add i18

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,16 +5,19 @@ import { classNames } from "shared/helpers/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { SideBar } from "widgets/SideBar";
+import { Suspense } from "react";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <SideBar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <SideBar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -2,11 +2,12 @@ import { classNames } from "shared/helpers/classNames/classNames";
 import cls from "./SideBar.module.scss";
 import { FC, useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher";
 interface SideBarProps {
   className?: string;
 }
 export const SideBar: FC<SideBarProps> = ({ className }) => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
@@ -19,7 +20,7 @@ export const SideBar: FC<SideBarProps> = ({ className }) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* <LangSwitcher /> */}
+        <LangSwitcher />
       </div>
     </div>
   );
