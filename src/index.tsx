3bd413267cf0dd8518dfaf0dff6ae2ba05a5f0cb
andre <cherepanovandrey07@gmail.com>
2024-02-25 16:17:58 +0300

    fix createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from 'app/App'
 import { BrowserRouter } from 'react-router-dom'
 import ThemeProvider from 'app/providers/ThemeProvider/ui/ThemeProvider'
 
 import 'shared/i18nconfig/i18n'
 
-render(
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
     <BrowserRouter>
         <ThemeProvider>
             <App />
         </ThemeProvider>
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
 )
